323762c66a8b112f40d393228e5dc206320281a9
PRITHWIRAJ SAMANTA <prskid1000@gmail.com>
2020-09-30 11:36:48 +0530

    Old files removed

--- app/send_message.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli=new mysqli("fdb21.awardspace.net","2734291_data","HATSoff@2018","2734291_data","3306");
-$data="list";
-$query="INSERT INTO app(id,sender,receiver,data) VALUES ('NULL','".$_GET['userid']."','".$_GET['receiver']."','".$_GET['data']."')";
-$result=$mysqli->query($query);
-$mysqli->close();
-?>
