cfe30c50ae5380417aa89ce6a3f7739ac75f3abf
PRITHWIRAJ SAMANTA <prskid1000@gmail.com>
2020-11-26 21:07:25 +0530

    Update remove_contact.php

--- Web-App/remove_contact.php
+++ Web-App/remove_contact.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli=new mysqli("fdb21.awardspace.net","2734291_data","HATSoff@2018","2734291_data","3306");
+$mysqli=new mysqli("fdb21.awardspace.net","2734291_data","password","2734291_data","3306");
 $query="DELETE FROM app WHERE sender='".$_GET['userid']."' AND receiver='".$_GET['receiver']."'";
 $result=$mysqli->query($query);
 ?>
